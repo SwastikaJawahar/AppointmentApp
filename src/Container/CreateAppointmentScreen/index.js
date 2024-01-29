2052b6d6ac8fc8a69f164e1f8102d0077147a029
SwastikaJawahar <136087727+SwastikaJawahar@users.noreply.github.com>
2024-01-29 09:53:21 +0000

    View doctor Profile

--- src/Container/CreateAppointmentScreen/index.js
+++ src/Container/CreateAppointmentScreen/index.js
@@ -6,8 +6,11 @@ import {
   TextInput,
   TouchableOpacity,
   FlatList,
+  Modal,
 } from 'react-native';
 import firestore from '@react-native-firebase/firestore';
+import MIcons from 'react-native-vector-icons/MaterialIcons';
+import Icons from 'react-native-vector-icons/Ionicons';
 
 const CreateAppointmentScreen = props => {
   const [searchText, setSearchText] = useState('');
@@ -68,16 +71,54 @@ const CreateAppointmentScreen = props => {
     );
     setSearchResults(filteredDoctors);
   };
+  const viewDoctorProfile = async doctor => {
+    try {
+      const userProfile = await getDoctorDataById(doctor.uid, 'UserProfile');
+      const qualifications = await getDoctorDataById(
+        doctor.uid,
+        'Qualification',
+      );
+      const experiences = await getDoctorDataById(doctor.uid, 'Experience');
 
+      const doctorProfile = {
+        userProfile,
+        qualifications,
+        experiences,
+      };
+
+      console.log(doctorProfile.userProfile);
+      console.log(doctorProfile.qualifications);
+      console.log(doctorProfile.experiences);
+
+      setDoctorProfile(doctorProfile);
+      setViewProfileModalVisible(true);
+    } catch (error) {
+      console.error('Error fetching doctor profile:', error);
+    }
+  };
   const renderDoctorItem = ({item}) => (
-    <View>
+    <View style={styles.renderDoctorItem}>
       <TouchableOpacity
         style={styles.doctorItem}
         onPress={() => setSelectedDoctor(item)}>
-        <Text style={styles.doctorName}>{item.name}</Text>
-        <Text style={styles.doctorDetails}>{item.specialty}</Text>
-        <Text style={styles.doctorDetails}>{item.location}</Text>
-        <Text style={styles.doctorDetails}>{item.contact}</Text>
+        <View style={styles.contentContainer}>
+          <View style={styles.leftContent}>
+            <Text style={styles.doctorName}>{item.name}</Text>
+            <Text style={styles.doctorDetails}>{item.specialty}</Text>
+            <Text style={styles.doctorDetails}>{item.location}</Text>
+            <Text style={styles.doctorDetails}>{item.contact}</Text>
+          </View>
+          <View style={styles.rightContent}>
+            <TouchableOpacity onPress={() => viewDoctorProfile(item)}>
+              <MIcons
+                style={styles.Icon}
+                name="person-search"
+                size={50}
+                color={'#046665'}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
         <TouchableOpacity
           style={styles.appointmentButton}
           onPress={() => {
@@ -108,6 +149,49 @@ const CreateAppointmentScreen = props => {
         renderItem={renderDoctorItem}
         ListEmptyComponent={<Text>No doctors found</Text>}
       />
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={viewProfileModalVisible}
+        onRequestClose={() => setViewProfileModalVisible(false)}>
+        <View style={styles.modalContainer}>
+          <Icons
+            style={styles.icon}
+            name="close"
+            onPress={() => setViewProfileModalVisible(false)}
+            color={'#046665'}
+            size={40}
+          />
+          <Text style={styles.modalHeading}>Doctor's Profile</Text>
+          {doctorProfile && (
+            <View>
+              <Text style={styles.doctorDetails}>
+                Name: {doctorProfile.userProfile[0]?.name}
+              </Text>
+              <Text style={styles.doctorDetails}>
+                Speciality: {doctorProfile.userProfile[0]?.specialty}
+              </Text>
+              <Text style={styles.doctorDetails}>
+                Contact: {doctorProfile.userProfile[0]?.contact}
+              </Text>
+              <Text style={styles.modalSubHeading}>Qualifications:</Text>
+              {doctorProfile.qualifications.map((qualification, index) => (
+                <Text style={styles.doctorDetails} key={index}>
+                  {qualification?.degreeName}
+                </Text>
+              ))}
+              <Text style={styles.modalSubHeading}>Experiences:</Text>
+              {doctorProfile.experiences.map((experience, index) => (
+                <View>
+                  <Text style={styles.doctorDetails} key={index}>
+                    {experience?.clinic}
+                  </Text>
+                </View>
+              ))}
+            </View>
+          )}
+        </View>
+      </Modal>
     </View>
   );
 };
@@ -119,15 +203,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#e3f1f1',
   },
-  DatePicker: {
-    marginTop: 16,
-    backgroundColor: '#fff',
-    borderRadius: 8,
-    elevation: 4,
+  contentContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   FlatList: {
     width: '100%',
     marginLeft: 70,
+  },
+  leftContent: {
+    flex: 1,
+  },
+  rightContent: {
+    // marginLeft: 30,
+    flex: 1,
   },
   searchInput: {
     height: 40,
@@ -139,6 +229,9 @@ const styles = StyleSheet.create({
     marginBottom: 16,
     backgroundColor: '#fff',
     marginTop: 10,
+  },
+  renderDoctorItem: {
+    flexDirection: 'row',
   },
   doctorItem: {
     width: '80%',
@@ -153,7 +246,7 @@ const styles = StyleSheet.create({
     marginBottom: 8,
   },
   doctorDetails: {
-    fontSize: 16,
+    fontSize: 20,
     marginBottom: 8,
   },
   appointmentButton: {
@@ -162,6 +255,43 @@ const styles = StyleSheet.create({
     paddingHorizontal: 16,
     borderRadius: 8,
     marginTop: 8,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ccc',
+  },
+  icon: {
+    marginLeft: '80%',
+    marginBottom: 10,
+  },
+  modalHeading: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  modalInput: {
+    height: 40,
+    width: '80%',
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 8,
+    paddingHorizontal: 10,
+    marginBottom: 16,
+    backgroundColor: '#fff',
+  },
+  modalButton: {
+    backgroundColor: '#27ae60',
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 8,
+    marginTop: 16,
+  },
+  modalSubHeading: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 10,
   },
   buttonText: {
     color: 'white',
